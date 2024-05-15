bf5bb7d0585adc6910698723e9cd91ba0afb06ef
Epitomaniac <d.mohebi1993@gmail.com>
2024-05-15 04:13:19 +0330

    Initial commit

---
+++ app/src/main/java/com/epitomaniac/textbookquiz/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.epitomaniac.textbookquiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.epitomaniac.textbookquiz.ui.theme.TextbookQuizTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      TextbookQuizTheme {
+        Surface(modifier = Modifier.fillMaxSize()) {
+          QuizScreen()
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun QuizScreen(modifier: Modifier = Modifier) {
+  val fontSize = 24.sp
+  Column(
+    modifier = modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+  ) {
+    Text(
+      text = "What is the capital of France?",
+      fontSize = fontSize
+    )
+    Text(text = "London", fontSize = fontSize)
+    Text(text = "Paris", fontSize = fontSize)
+    Text(text = "Rome", fontSize = fontSize)
+    Text(text = "Munich", fontSize = fontSize)
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  TextbookQuizTheme {
+    QuizScreen()
+  }
+}
